77f5d65207a61c0fd30c5af3f72fb8564f43ec31
Jaremy Creechley <creechley@gmail.com>
2025-07-26 03:05:18 -0600

    setup onnx

--- src/nonnx/private.nim
+++ src/nonnx/private.nim
@@ -111,12 +111,12 @@ type
     CreateStatus*: proc(code: OrtErrorCode, msg: cstring): ptr OrtStatus
     GetErrorCode*: proc(status: ptr OrtStatus): OrtErrorCode
     GetErrorMessage*: proc(status: ptr OrtStatus): cstring
-    CreateEnv*: proc(log_severity_level: OrtLoggingLevel, logid: cstring, out: ptr ptr OrtEnv): ptr OrtStatus
-    CreateEnvWithCustomLogger*: proc(logging_function: proc(param: pointer, severity: OrtLoggingLevel, category: cstring, logid: cstring, code_location: cstring, message: cstring), logger_param: pointer, log_severity_level: OrtLoggingLevel, logid: cstring, out: ptr ptr OrtEnv): ptr OrtStatus
+    CreateEnv*: proc(log_severity_level: OrtLoggingLevel, logid: cstring, outs: ptr ptr OrtEnv): ptr OrtStatus
+    CreateEnvWithCustomLogger*: proc(logging_function: proc(param: pointer, severity: OrtLoggingLevel, category: cstring, logid: cstring, code_location: cstring, message: cstring), logger_param: pointer, log_severity_level: OrtLoggingLevel, logid: cstring, outs: ptr ptr OrtEnv): ptr OrtStatus
     EnableTelemetryEvents*: proc(env: ptr OrtEnv): ptr OrtStatus
     DisableTelemetryEvents*: proc(env: ptr OrtEnv): ptr OrtStatus
-    CreateSession*: proc(env: ptr OrtEnv, model_path: cstring, options: ptr OrtSessionOptions, out: ptr ptr OrtSession): ptr OrtStatus
-    CreateSessionFromArray*: proc(env: ptr OrtEnv, model_data: pointer, model_data_length: csize, options: ptr OrtSessionOptions, out: ptr ptr OrtSession): ptr OrtStatus
+    CreateSession*: proc(env: ptr OrtEnv, model_path: cstring, options: ptr OrtSessionOptions, outs: ptr ptr OrtSession): ptr OrtStatus
+    CreateSessionFromArray*: proc(env: ptr OrtEnv, model_data: pointer, model_data_length: csize, options: ptr OrtSessionOptions, outs: ptr ptr OrtSession): ptr OrtStatus
     Run*: proc(session: ptr OrtSession, run_options: ptr OrtRunOptions, input_names: ptr cstring, inputs: ptr (ptr OrtValue), input_len: csize, output_names: ptr cstring, output_names_len: csize, outputs: ptr (ptr OrtValue)): ptr OrtStatus
     CreateSessionOptions*: proc(options: ptr ptr OrtSessionOptions): ptr OrtStatus
     SetOptimizedModelFilePath*: proc(options: ptr OrtSessionOptions, optimized_model_filepath: cstring): ptr OrtStatus
@@ -134,20 +134,20 @@ type
     SetSessionGraphOptimizationLevel*: proc(options: ptr OrtSessionOptions, graph_optimization_level: cint): ptr OrtStatus
     SetIntraOpNumThreads*: proc(options: ptr OrtSessionOptions, intra_op_num_threads: cint): ptr OrtStatus
     SetInterOpNumThreads*: proc(options: ptr OrtSessionOptions, inter_op_num_threads: cint): ptr OrtStatus
-    CreateCustomOpDomain*: proc(domain: cstring, out: ptr ptr OrtCustomOpDomain): ptr OrtStatus
+    CreateCustomOpDomain*: proc(domain: cstring, outs: ptr ptr OrtCustomOpDomain): ptr OrtStatus
     CustomOpDomain_Add*: proc(custom_op_domain: ptr OrtCustomOpDomain, op: ptr OrtOp): ptr OrtStatus
     AddCustomOpDomain*: proc(options: ptr OrtSessionOptions, custom_op_domain: ptr OrtCustomOpDomain): ptr OrtStatus
     RegisterCustomOpsLibrary*: proc(options: ptr OrtSessionOptions, library_path: cstring, library_handle: ptr pointer): ptr OrtStatus
-    SessionGetInputCount*: proc(session: ptr OrtSession, out: ptr csize): ptr OrtStatus
-    SessionGetOutputCount*: proc(session: ptr OrtSession, out: ptr csize): ptr OrtStatus
-    SessionGetOverridableInitializerCount*: proc(session: ptr OrtSession, out: ptr csize): ptr OrtStatus
+    SessionGetInputCount*: proc(session: ptr OrtSession, outs: ptr csize): ptr OrtStatus
+    SessionGetOutputCount*: proc(session: ptr OrtSession, outs: ptr csize): ptr OrtStatus
+    SessionGetOverridableInitializerCount*: proc(session: ptr OrtSession, outs: ptr csize): ptr OrtStatus
     SessionGetInputTypeInfo*: proc(session: ptr OrtSession, index: csize, type_info: ptr ptr OrtTypeInfo): ptr OrtStatus
     SessionGetOutputTypeInfo*: proc(session: ptr OrtSession, index: csize, type_info: ptr ptr OrtTypeInfo): ptr OrtStatus
     SessionGetOverridableInitializerTypeInfo*: proc(session: ptr OrtSession, index: csize, type_info: ptr ptr OrtTypeInfo): ptr OrtStatus
-    SessionGetInputName*: proc(session: ptr OrtSession, index: csize, allocator: ptr OrtAllocator, value: ptr cstring): ptr OrtStatus
-    SessionGetOutputName*: proc(session: ptr OrtSession, index: csize, allocator: ptr OrtAllocator, value: ptr cstring): ptr OrtStatus
-    SessionGetOverridableInitializerName*: proc(session: ptr OrtSession, index: csize, allocator: ptr OrtAllocator, value: ptr cstring): ptr OrtStatus
-    CreateRunOptions*: proc(out: ptr ptr OrtRunOptions): ptr OrtStatus
+    SessionGetInputName*: proc(session: ptr OrtSession, index: csize, allocator: ptr OrtAllocator, outs: ptr cstring): ptr OrtStatus
+    SessionGetOutputName*: proc(session: ptr OrtSession, index: csize, allocator: ptr OrtAllocator, outs: ptr cstring): ptr OrtStatus
+    SessionGetOverridableInitializerName*: proc(session: ptr OrtSession, index: csize, allocator: ptr OrtAllocator, outs: ptr cstring): ptr OrtStatus
+    CreateRunOptions*: proc(outs: ptr ptr OrtRunOptions): ptr OrtStatus
     RunOptionsSetRunLogVerbosityLevel*: proc(options: ptr OrtRunOptions, log_verbosity_level: cint): ptr OrtStatus
     RunOptionsSetRunLogSeverityLevel*: proc(options: ptr OrtRunOptions, log_severity_level: cint): ptr OrtStatus
     RunOptionsSetRunTag*: proc(options: ptr OrtRunOptions, run_tag: cstring): ptr OrtStatus
@@ -156,50 +156,50 @@ type
     RunOptionsGetRunTag*: proc(options: ptr OrtRunOptions, run_tag: ptr cstring): ptr OrtStatus
     RunOptionsSetTerminate*: proc(options: ptr OrtRunOptions): ptr OrtStatus
     RunOptionsUnsetTerminate*: proc(options: ptr OrtRunOptions): ptr OrtStatus
-    CreateTensorAsOrtValue*: proc(allocator: ptr OrtAllocator, shape: ptr int64, shape_len: csize, `type`: ONNXTensorElementDataType, out: ptr ptr OrtValue): ptr OrtStatus
-    CreateTensorWithDataAsOrtValue*: proc(info: ptr OrtMemoryInfo, p_data: pointer, p_data_len: csize, shape: ptr int64, shape_len: csize, `type`: ONNXTensorElementDataType, out: ptr ptr OrtValue): ptr OrtStatus
-    IsTensor*: proc(value: ptr OrtValue, out: ptr cint): ptr OrtStatus
-    GetTensorMutableData*: proc(value: ptr OrtValue, out: ptr pointer): ptr OrtStatus
+    CreateTensorAsOrtValue*: proc(allocator: ptr OrtAllocator, shape: ptr int64, shape_len: csize, `type`: ONNXTensorElementDataType, outs: ptr ptr OrtValue): ptr OrtStatus
+    CreateTensorWithDataAsOrtValue*: proc(info: ptr OrtMemoryInfo, p_data: pointer, p_data_len: csize, shape: ptr int64, shape_len: csize, `type`: ONNXTensorElementDataType, outs: ptr ptr OrtValue): ptr OrtStatus
+    IsTensor*: proc(value: ptr OrtValue, outs: ptr cint): ptr OrtStatus
+    GetTensorMutableData*: proc(value: ptr OrtValue, outs: ptr pointer): ptr OrtStatus
     FillStringTensor*: proc(value: ptr OrtValue, s: ptr cstring, s_len: csize): ptr OrtStatus
     GetStringTensorDataLength*: proc(value: ptr OrtValue, len: ptr csize): ptr OrtStatus
     GetStringTensorContent*: proc(value: ptr OrtValue, s: pointer, s_len: csize, offsets: ptr csize, offsets_len: csize): ptr OrtStatus
-    CastTypeInfoToTensorInfo*: proc(type_info: ptr OrtTypeInfo, out: ptr (ptr OrtTensorTypeAndShapeInfo)): ptr OrtStatus
-    GetOnnxTypeFromTypeInfo*: proc(type_info: ptr OrtTypeInfo, out: ptr ONNXType): ptr OrtStatus
-    CreateTensorTypeAndShapeInfo*: proc(out: ptr ptr OrtTensorTypeAndShapeInfo): ptr OrtStatus
+    CastTypeInfoToTensorInfo*: proc(type_info: ptr OrtTypeInfo, outs: ptr (ptr OrtTensorTypeAndShapeInfo)): ptr OrtStatus
+    GetOnnxTypeFromTypeInfo*: proc(type_info: ptr OrtTypeInfo, outs: ptr ONNXType): ptr OrtStatus
+    CreateTensorTypeAndShapeInfo*: proc(outs: ptr ptr OrtTensorTypeAndShapeInfo): ptr OrtStatus
     SetTensorElementType*: proc(info: ptr OrtTensorTypeAndShapeInfo, `type`: ONNXTensorElementDataType): ptr OrtStatus
     SetDimensions*: proc(info: ptr OrtTensorTypeAndShapeInfo, dim_values: ptr int64, dim_count: csize): ptr OrtStatus
-    GetTensorElementType*: proc(info: ptr OrtTensorTypeAndShapeInfo, out: ptr ONNXTensorElementDataType): ptr OrtStatus
-    GetDimensionsCount*: proc(info: ptr OrtTensorTypeAndShapeInfo, out: ptr csize): ptr OrtStatus
+    GetTensorElementType*: proc(info: ptr OrtTensorTypeAndShapeInfo, outs: ptr ONNXTensorElementDataType): ptr OrtStatus
+    GetDimensionsCount*: proc(info: ptr OrtTensorTypeAndShapeInfo, outs: ptr csize): ptr OrtStatus
     GetDimensions*: proc(info: ptr OrtTensorTypeAndShapeInfo, dim_values: ptr int64, dim_values_length: csize): ptr OrtStatus
     GetSymbolicDimensions*: proc(info: ptr OrtTensorTypeAndShapeInfo, dim_params: ptr cstring, dim_params_length: csize): ptr OrtStatus
-    GetTensorShapeElementCount*: proc(info: ptr OrtTensorTypeAndShapeInfo, out: ptr csize): ptr OrtStatus
-    GetTensorTypeAndShape*: proc(value: ptr OrtValue, out: ptr ptr OrtTensorTypeAndShapeInfo): ptr OrtStatus
-    GetTypeInfo*: proc(value: ptr OrtValue, out: ptr ptr OrtTypeInfo): ptr OrtStatus
-    GetValueType*: proc(value: ptr OrtValue, out: ptr ONNXType): ptr OrtStatus
-    CreateMemoryInfo*: proc(name: cstring, `type`: cint, id: cint, mem_type: cint, out: ptr ptr OrtMemoryInfo): ptr OrtStatus
-    CreateCpuMemoryInfo*: proc(`type`: cint, mem_type: cint, out: ptr ptr OrtMemoryInfo): ptr OrtStatus
-    CompareMemoryInfo*: proc(info1: ptr OrtMemoryInfo, info2: ptr OrtMemoryInfo, out: ptr cint): ptr OrtStatus
-    MemoryInfoGetName*: proc(ptr: ptr OrtMemoryInfo, out: ptr cstring): ptr OrtStatus
-    MemoryInfoGetId*: proc(ptr: ptr OrtMemoryInfo, out: ptr cint): ptr OrtStatus
-    MemoryInfoGetMemType*: proc(ptr: ptr OrtMemoryInfo, out: ptr cint): ptr OrtStatus
-    MemoryInfoGetType*: proc(ptr: ptr OrtMemoryInfo, out: ptr cint): ptr OrtStatus
-    AllocatorAlloc*: proc(ort_allocator: ptr OrtAllocator, size: csize, out: ptr pointer): ptr OrtStatus
+    GetTensorShapeElementCount*: proc(info: ptr OrtTensorTypeAndShapeInfo, outs: ptr csize): ptr OrtStatus
+    GetTensorTypeAndShape*: proc(value: ptr OrtValue, outs: ptr ptr OrtTensorTypeAndShapeInfo): ptr OrtStatus
+    GetTypeInfo*: proc(value: ptr OrtValue, outs: ptr ptr OrtTypeInfo): ptr OrtStatus
+    GetValueType*: proc(value: ptr OrtValue, outs: ptr ONNXType): ptr OrtStatus
+    CreateMemoryInfo*: proc(name: cstring, `type`: cint, id: cint, mem_type: cint, outs: ptr ptr OrtMemoryInfo): ptr OrtStatus
+    CreateCpuMemoryInfo*: proc(`type`: cint, mem_type: cint, outs: ptr ptr OrtMemoryInfo): ptr OrtStatus
+    CompareMemoryInfo*: proc(info1: ptr OrtMemoryInfo, info2: ptr OrtMemoryInfo, outs: ptr cint): ptr OrtStatus
+    MemoryInfoGetName*: proc(info: ptr OrtMemoryInfo, outs: ptr cstring): ptr OrtStatus
+    MemoryInfoGetId*: proc(info: ptr OrtMemoryInfo, outs: ptr cint): ptr OrtStatus
+    MemoryInfoGetMemType*: proc(info: ptr OrtMemoryInfo, outs: ptr cint): ptr OrtStatus
+    MemoryInfoGetType*: proc(info: ptr OrtMemoryInfo, outs: ptr cint): ptr OrtStatus
+    AllocatorAlloc*: proc(ort_allocator: ptr OrtAllocator, size: csize, outs: ptr pointer): ptr OrtStatus
     AllocatorFree*: proc(ort_allocator: ptr OrtAllocator, p: pointer): ptr OrtStatus
-    AllocatorGetInfo*: proc(ort_allocator: ptr OrtAllocator, out: ptr (ptr OrtMemoryInfo)): ptr OrtStatus
-    GetAllocatorWithDefaultOptions*: proc(out: ptr ptr OrtAllocator): ptr OrtStatus
+    AllocatorGetInfo*: proc(ort_allocator: ptr OrtAllocator, outs: ptr (ptr OrtMemoryInfo)): ptr OrtStatus
+    GetAllocatorWithDefaultOptions*: proc(outs: ptr ptr OrtAllocator): ptr OrtStatus
     AddFreeDimensionOverride*: proc(options: ptr OrtSessionOptions, dim_denotation: cstring, dim_value: int64): ptr OrtStatus
-    GetValue*: proc(value: ptr OrtValue, index: cint, allocator: ptr OrtAllocator, out: ptr ptr OrtValue): ptr OrtStatus
-    GetValueCount*: proc(value: ptr OrtValue, out: ptr csize): ptr OrtStatus
-    CreateValue*: proc(in_val: ptr (ptr OrtValue), num_values: csize, value_type: ONNXType, out: ptr ptr OrtValue): ptr OrtStatus
-    CreateOpaqueValue*: proc(domain_name: cstring, type_name: cstring, data_container: pointer, data_container_size: csize, out: ptr ptr OrtValue): ptr OrtStatus
+    GetValue*: proc(value: ptr OrtValue, index: cint, allocator: ptr OrtAllocator, outs: ptr ptr OrtValue): ptr OrtStatus
+    GetValueCount*: proc(value: ptr OrtValue, outs: ptr csize): ptr OrtStatus
+    CreateValue*: proc(in_val: ptr (ptr OrtValue), num_values: csize, value_type: ONNXType, outs: ptr ptr OrtValue): ptr OrtStatus
+    CreateOpaqueValue*: proc(domain_name: cstring, type_name: cstring, data_container: pointer, data_container_size: csize, outs: ptr ptr OrtValue): ptr OrtStatus
     GetOpaqueValue*: proc(domain_name: cstring, type_name: cstring, in_val: ptr OrtValue, data_container: pointer, data_container_size: csize): ptr OrtStatus
-    KernelInfoGetAttribute_float*: proc(info: ptr OrtOp, name: cstring, out: ptr float): ptr OrtStatus
-    KernelInfoGetAttribute_int64*: proc(info: ptr OrtOp, name: cstring, out: ptr int64): ptr OrtStatus
-    KernelInfoGetAttribute_string*: proc(info: ptr OrtOp, name: cstring, out: cstring, size: ptr csize): ptr OrtStatus
-    KernelContext_GetInputCount*: proc(context: ptr OrtOp, out: ptr csize): ptr OrtStatus
-    KernelContext_GetOutputCount*: proc(context: ptr OrtOp, out: ptr csize): ptr OrtStatus
-    KernelContext_GetInput*: proc(context: ptr OrtOp, index: csize, out: ptr (ptr OrtValue)): ptr OrtStatus
-    KernelContext_GetOutput*: proc(context: ptr OrtOp, index: csize, dim_values: ptr int64, dim_count: csize, out: ptr ptr OrtValue): ptr OrtStatus
+    KernelInfoGetAttribute_float*: proc(info: ptr OrtOp, name: cstring, outs: ptr float): ptr OrtStatus
+    KernelInfoGetAttribute_int64*: proc(info: ptr OrtOp, name: cstring, outs: ptr int64): ptr OrtStatus
+    KernelInfoGetAttribute_string*: proc(info: ptr OrtOp, name: cstring, outs: cstring, size: ptr csize): ptr OrtStatus
+    KernelContext_GetInputCount*: proc(context: ptr OrtOp, outs: ptr csize): ptr OrtStatus
+    KernelContext_GetOutputCount*: proc(context: ptr OrtOp, outs: ptr csize): ptr OrtStatus
+    KernelContext_GetInput*: proc(context: ptr OrtOp, index: csize, outs: ptr (ptr OrtValue)): ptr OrtStatus
+    KernelContext_GetOutput*: proc(context: ptr OrtOp, index: csize, dim_values: ptr int64, dim_count: csize, outs: ptr ptr OrtValue): ptr OrtStatus
     ReleaseEnv*: proc(input: ptr OrtEnv)
     ReleaseStatus*: proc(input: ptr OrtStatus)
     ReleaseMemoryInfo*: proc(input: ptr OrtMemoryInfo)
@@ -212,8 +212,8 @@ type
     ReleaseCustomOpDomain*: proc(input: ptr OrtCustomOpDomain)
     ReleaseMapTypeInfo*: proc(input: ptr OrtMapTypeInfo)
     ReleaseSequenceTypeInfo*: proc(input: ptr OrtSequenceTypeInfo)
-    SessionEndProfiling*: proc(session: ptr OrtSession, allocator: ptr OrtAllocator, out: ptr cstring): ptr OrtStatus
-    SessionGetModelMetadata*: proc(session: ptr OrtSession, out: ptr ptr OrtModelMetadata): ptr OrtStatus
+    SessionEndProfiling*: proc(session: ptr OrtSession, allocator: ptr OrtAllocator, outs: ptr cstring): ptr OrtStatus
+    SessionGetModelMetadata*: proc(session: ptr OrtSession, outs: ptr ptr OrtModelMetadata): ptr OrtStatus
     ModelMetadataGetProducerName*: proc(model_metadata: ptr OrtModelMetadata, allocator: ptr OrtAllocator, value: ptr cstring): ptr OrtStatus
     ModelMetadataGetGraphName*: proc(model_metadata: ptr OrtModelMetadata, allocator: ptr OrtAllocator, value: ptr cstring): ptr OrtStatus
     ModelMetadataGetDomain*: proc(model_metadata: ptr OrtModelMetadata, allocator: ptr OrtAllocator, value: ptr cstring): ptr OrtStatus
@@ -221,22 +221,22 @@ type
     ModelMetadataLookupCustomMetadataMap*: proc(model_metadata: ptr OrtModelMetadata, allocator: ptr OrtAllocator, key: cstring, value: ptr cstring): ptr OrtStatus
     ModelMetadataGetVersion*: proc(model_metadata: ptr OrtModelMetadata, value: ptr int64): ptr OrtStatus
     ReleaseModelMetadata*: proc(input: ptr OrtModelMetadata)
-    CreateEnvWithGlobalThreadPools*: proc(log_severity_level: OrtLoggingLevel, logid: cstring, tp_options: ptr OrtThreadingOptions, out: ptr ptr OrtEnv): ptr OrtStatus
+    CreateEnvWithGlobalThreadPools*: proc(log_severity_level: OrtLoggingLevel, logid: cstring, tp_options: ptr OrtThreadingOptions, outs: ptr ptr OrtEnv): ptr OrtStatus
     DisablePerSessionThreads*: proc(options: ptr OrtSessionOptions): ptr OrtStatus
-    CreateThreadingOptions*: proc(out: ptr ptr OrtThreadingOptions): ptr OrtStatus
+    CreateThreadingOptions*: proc(outs: ptr ptr OrtThreadingOptions): ptr OrtStatus
     ReleaseThreadingOptions*: proc(input: ptr OrtThreadingOptions)
     ModelMetadataGetCustomMetadataMapKeys*: proc(model_metadata: ptr OrtModelMetadata, allocator: ptr OrtAllocator, keys: ptr (ptr cstring), num_keys: ptr int64): ptr OrtStatus
     AddFreeDimensionOverrideByName*: proc(options: ptr OrtSessionOptions, dim_name: cstring, dim_value: int64): ptr OrtStatus
-    GetAvailableProviders*: proc(out_ptr: ptr (ptr cstring), provider_length: ptr cint): ptr OrtStatus
-    ReleaseAvailableProviders*: proc(ptr: ptr cstring, providers_length: cint): ptr OrtStatus
-    GetStringTensorElementLength*: proc(value: ptr OrtValue, index: csize, out: ptr csize): ptr OrtStatus
+    GetAvailableProviders*: proc(outs: ptr (ptr cstring), provider_length: ptr cint): ptr OrtStatus
+    ReleaseAvailableProviders*: proc(input: ptr cstring, providers_length: cint): ptr OrtStatus
+    GetStringTensorElementLength*: proc(value: ptr OrtValue, index: csize, outs: ptr csize): ptr OrtStatus
     GetStringTensorElement*: proc(value: ptr OrtValue, s_len: csize, index: csize, s: pointer): ptr OrtStatus
     FillStringTensorElement*: proc(value: ptr OrtValue, s: cstring, index: csize): ptr OrtStatus
     AddSessionConfigEntry*: proc(options: ptr OrtSessionOptions, config_key: cstring, config_value: cstring): ptr OrtStatus
-    CreateAllocator*: proc(session: ptr OrtSession, mem_info: ptr OrtMemoryInfo, out: ptr ptr OrtAllocator): ptr OrtStatus
+    CreateAllocator*: proc(session: ptr OrtSession, mem_info: ptr OrtMemoryInfo, outs: ptr ptr OrtAllocator): ptr OrtStatus
     ReleaseAllocator*: proc(input: ptr OrtAllocator)
     RunWithBinding*: proc(session: ptr OrtSession, run_options: ptr OrtRunOptions, binding_ptr: ptr OrtIoBinding): ptr OrtStatus
-    CreateIoBinding*: proc(session: ptr OrtSession, out: ptr ptr OrtIoBinding): ptr OrtStatus
+    CreateIoBinding*: proc(session: ptr OrtSession, outs: ptr ptr OrtIoBinding): ptr OrtStatus
     ReleaseIoBinding*: proc(input: ptr OrtIoBinding)
     BindInput*: proc(binding_ptr: ptr OrtIoBinding, name: cstring, val_ptr: ptr OrtValue): ptr OrtStatus
     BindOutput*: proc(binding_ptr: ptr OrtIoBinding, name: cstring, val_ptr: ptr OrtValue): ptr OrtStatus
@@ -245,10 +245,10 @@ type
     GetBoundOutputValues*: proc(binding_ptr: ptr OrtIoBinding, allocator: ptr OrtAllocator, output: ptr (ptr (ptr OrtValue)), output_count: ptr csize): ptr OrtStatus
     ClearBoundInputs*: proc(binding_ptr: ptr OrtIoBinding)
     ClearBoundOutputs*: proc(binding_ptr: ptr OrtIoBinding)
-    TensorAt*: proc(value: ptr OrtValue, location_values: ptr int64, location_values_count: csize, out: ptr pointer): ptr OrtStatus
+    TensorAt*: proc(value: ptr OrtValue, location_values: ptr int64, location_values_count: csize, outs: ptr pointer): ptr OrtStatus
     CreateAndRegisterAllocator*: proc(env: ptr OrtEnv, mem_info: ptr OrtMemoryInfo, arena_cfg: ptr OrtArenaCfg): ptr OrtStatus
     SetLanguageProjection*: proc(ort_env: ptr OrtEnv, projection: cint): ptr OrtStatus
-    SessionGetProfilingStartTimeNs*: proc(session: ptr OrtSession, out: ptr uint64): ptr OrtStatus
+    SessionGetProfilingStartTimeNs*: proc(session: ptr OrtSession, outs: ptr uint64): ptr OrtStatus
     SetGlobalIntraOpNumThreads*: proc(tp_options: ptr OrtThreadingOptions, intra_op_num_threads: cint): ptr OrtStatus
     SetGlobalInterOpNumThreads*: proc(tp_options: ptr OrtThreadingOptions, inter_op_num_threads: cint): ptr OrtStatus
 
